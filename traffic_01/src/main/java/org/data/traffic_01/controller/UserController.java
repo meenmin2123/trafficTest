37cea4ee66fa5a24a22e1cc325af8c455a42bbf5
meenmin2123 <min1016alsrud@gmail.com>
2024-12-22 00:00:10 +0900

    socailLogin - google 연결

---
+++ traffic_01/src/main/java/org/data/traffic_01/controller/UserController.java
@@ -0,0 +1,18 @@
+package org.data.traffic_01.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/user")
+@RequiredArgsConstructor
+public class UserController {
+    private final UserService userService;
+
+    @PostMapping("social-login")
+    public ResponseEntity<LoginResponse> doSocialLogin 
+
+}
